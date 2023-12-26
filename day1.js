66e3fe3583d9a1d35b03953507755cd4fb7d9b23
nguyenpnguyen <nguyen.pn@proton.me>
2023-12-27 00:32:47 +0700

    day 1 updated

--- day1.js
+++ day1.js
@@ -1,18 +1,9 @@
 const fs = require("fs");
 
-const input = fs.readFileSync("./input/d1", "utf-8").split("\n");
-
-const numMap = new Map();
-
-numMap.set("one", 1);
-numMap.set("two", 2);
-numMap.set("three", 3);
-numMap.set("four", 4);
-numMap.set("five", 5);
-numMap.set("six", 6);
-numMap.set("seven", 7);
-numMap.set("eight", 8);
-numMap.set("nine", 9);
+const input = fs
+  .readFileSync("./input/d1.txt", "utf-8")
+  .split("\n")
+  .map((line) => line.trim(""));
 
 function getDigits(str) {
   let result = "";
@@ -24,5 +15,37 @@ function getDigits(str) {
   return result;
 }
 
-input.forEach((line) => {});
-console.log(numMap.entries());
+function replaceNumberSubstring(inputString) {
+  const numberMap = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  // Replace all occurrences of the keys of numberMap with their values;
+  // Remember edge cases
+}
+
+let sum = 0;
+
+input.forEach((line) => {
+  console.log(line);
+  const numberString = replaceNumberSubstring(line);
+  console.log(numberString);
+  const digits = getDigits(numberString);
+  console.log(digits);
+  let number = parseInt(digits[0] + digits[digits.length - 1]);
+  if (isNaN(number)) {
+    number = 0;
+  }
+  console.log(number, "\n");
+  sum += number;
+});
+
+console.log("Sum: ", sum);
